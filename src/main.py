f053048987014b1a68c865f8ef2a5263520e1c4c
Daniel Y <33942379+qanelph@users.noreply.github.com>
2022-06-30 15:44:52 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from supervisely.app.fastapi import available_after_shutdown
 
 @g.app.get("/")
 @available_after_shutdown(app=g.app)
-def read_index(request: Request):
+def read_index(request: Request = None):
     return g.templates_env.TemplateResponse('index.html', {'request': request,
                                                            'smart_tool': SmartTool})
 
